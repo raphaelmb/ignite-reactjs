f17cedb4797dbdbdff2e6d0788c75d0cb44dadd8
Raphael Berger <raphaelmb@gmail.com>
2022-07-18 11:40:00 -0300

    Add Post component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,9 @@
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -10,7 +11,10 @@ export function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-        <main></main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
